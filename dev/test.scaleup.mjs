d4d5c417db15e5083f1bde5b343b8bc0c11c4254
yandeu <20306025+yandeu@users.noreply.github.com>
2021-07-03 15:26:35 +0200

    Update dev scripts

--- dev/test.scaleup.mjs
+++ dev/test.scaleup.mjs
@@ -1,5 +1,5 @@
 import { docker } from '../dist/lib/docker.js'
-import { executeTask } from '../dist/tasks/tasks.js'
+import { executeTask } from '../dist/tasks/task.autoscale.js'
 
 const serviceID = 'oqjuq4qzfmw9'
 
@@ -7,8 +7,8 @@ const service = await docker(`services/${serviceID}`)
 const replicas = service?.Spec?.Mode?.Replicated?.Replicas
 
 if (typeof replicas === 'number' && replicas > 0) {
-    let ratio
+  let ratio
 
-    ratio = 1
-    if (ratio - 0.5 > 0) executeTask(service, { name: 'SCALE_UP', autoscaler: { min: 1, max: 20 } })
+  ratio = 1
+  if (ratio - 0.5 > 0) executeTask(service, { name: 'SCALE_UP', autoscaler: { min: 1, max: 20 } })
 }
